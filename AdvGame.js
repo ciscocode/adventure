44b262a3f231b3dd269e610fc9ac09be78aa1f0e
Cisco <fmontene92@gmail.com>
2020-04-17 13:13:43 -0700

    player begins w/ h20 & fix drop/take bug

--- AdvGame.js
+++ AdvGame.js
@@ -27,7 +27,7 @@ function AdvGame() {
    let gameStatus = "" 
 
    //distirbute objects into their respective rooms
-   distributeObjects(objects,rooms)
+   distributeObjects(objects,rooms,inventory)
 
    let element = document.getElementById("GameData");
    if (element === null) return undefined;
@@ -55,7 +55,7 @@ function AdvGame() {
    //this function checks the line the user inputs.
    function checkAnswer(line) {
 
-      let actionVerbs = ["quit","help","look","take","drop","inventory"] //this is a list of actionverbs
+      let actionVerbs = ["quit","help","look","take","drop","inventory","cheat"] //this is a list of actionverbs
       let actionVerbBoolean = false // begin as false. if we happen to identify a word as an actionVerb then we can make this true
       
       line = line.toLowerCase() //made the input line lowercase
@@ -153,86 +153,107 @@ function AdvGame() {
 
        }
 
-          let currentRoomPassages = currentRoom.getPassages()
          //  console.log(currentRoomPassages)
 
 
             //console.log('hi')
             // console.log(currentRoom.getPassages()[i].getDirection())
+            let currentRoomPassages = currentRoom.getPassages()
 
-            //if the passage of the room is forced
-            if (currentRoomPassages[0].getDirection() === "forced") { //a forced passage will always be identified in the first index of the passage array
-               // console.log("this is a forced passage")
-               // console.log(currentRoomPassages[i].getDestinationRoom())
+      forcedPassages(currentRoomPassages, inventory, rooms )
 
-               // line = "forced" //i am acting as if forced was the input
-               // checkAnswer(line) //call the function all over again with "forced" as my input
-               // console.log("the next line will describe room")
-               describeRoom()
-               // console.log(" ") //adds a space
+            
+   }
 
-                   //check to see if the user is holding a key or not
-               //if the user has a key that matches with a passage then you must send that user to that specific forced passage
+   function forcedPassages(currentRoomPassages, inventory, rooms ) {
 
-               //TODO change to indexofforcedpassage
-               let indexOfForcedKey 
-               // console.log('before conditional begins')
+       //if the passage of the room is forced
+       if (currentRoomPassages[0].getDirection() === "forced") { //a forced passage will always be identified in the first index of the passage array
+         // console.log("this is a forced passage")
+         // console.log(currentRoomPassages[i].getDestinationRoom())
 
-               //if there is only one forced passage then the index will be 0 because there is only one passage
-               if (currentRoomPassages.length === 1)  {
-                  // console.log("puppy")
-                  indexOfForcedKey = 0
-               }  
+         // line = "forced" //i am acting as if forced was the input
+         // checkAnswer(line) //call the function all over again with "forced" as my input
+         // console.log("the next line will describe room")
+         describeRoom()
+         // console.log(" ") //adds a space
 
-               else {
+             //check to see if the user is holding a key or not
+         //if the user has a key that matches with a passage then you must send that user to that specific forced passage
 
-                  if (inventory.length > 0) {
+         //TODO change to indexofforcedpassage
+         let indexOfForcedKey 
+         // console.log('before conditional begins')
 
-                     //this loop checks my inventory. If an item in my inventory 
-                     for (let i=0; i<inventory.length; i++) {
-                        // console.log('outer loop works')
+         //if there is only one forced passage then the index will be 0 because there is only one passage
+         if (currentRoomPassages.length === 1)  {
+            // console.log("puppy")
+            indexOfForcedKey = 0
+         }  
 
-                        for (let j=0; j<currentRoomPassages.length; j++) {
-                           // console.log("inner loop is working")
-                           // console.log("A " + inventory[i].getName().toLowerCase())
-                           // console.log(currentRoomPassages)
+         else {
 
-                           //passages = currentRoom.getPassages()
-                           //passages[arrayOfMatchingDirectionPassagesIndecies[j]].getKey()
+            if (inventory.length > 0) {
+               //console.log("hi")
 
-                           // console.log(currentRoomPassages[j])
-                           // console.log(currentRoomPassages[j].getKey())
-                           // console.log("B " + currentRoomPassages[j].getKey().toLowerCase())
+               //this loop checks my inventory. If an item in my inventory 
+               for (let i=0; i<inventory.length; i++) {
+                   //console.log('outer loop works')
+               
 
-                           //if there is in fact a key attached to the passage
-                           if (currentRoomPassages[j].getKey() !== undefined) {
-                              // console.log("if there is a key")
+                  for (let j=0; j<currentRoomPassages.length; j++) {
+                     //console.log("inner loop is working")
+                     // console.log("A " + inventory[i].getName().toLowerCase())
+                     // console.log(currentRoomPassages)
 
-                              //check to see if the items in your inventory match the key
-                              if (inventory[i].getName().toLowerCase() === currentRoomPassages[j].getKey().toLowerCase()) {
-                                 // console.log("inside my if statement")
-                                 indexOfForcedKey = 0
-                              }
-                           }
+                     //passages = currentRoom.getPassages()
+                     //passages[arrayOfMatchingDirectionPassagesIndecies[j]].getKey()
+
+                     // console.log(currentRoomPassages[j])
+                     // console.log(currentRoomPassages[j].getKey())
+                     // console.log("B " + currentRoomPassages[j].getKey().toLowerCase())
+
+                     //if there is in fact a key attached to the passage
+                     if (currentRoomPassages[j].getKey() !== undefined) {
+                        //console.log("if there is a key")
+                        // console.log(inventory[i].getName().toLowerCase())
+                        // console.log(currentRoomPassages[j].getKey().toLowerCase())
+
+                        //check to see if the items in your inventory match the key
+                        if (inventory[i].getName().toLowerCase() === currentRoomPassages[j].getKey().toLowerCase()) {
+                           //console.log("inside my if statement")
+                           indexOfForcedKey = 0
+                        }
+                        //if it doesnt match the key then the index of the forced key is 1
+                        else {
+                           indexOfForcedKey = 1
                         }
                      }
                   }
-
-                  //if a person doesn't have a key then the forced index is one. All forced passages with a key have an index of 0
-                  else {
-                     indexOfForcedKey = 1
-                 }
-               } 
-               // console.log("this is the index of the forced passage " + indexOfForcedKey)
-               // console.log("this is the current room " + currentRoomPassages[indexOfForcedKey].getDestinationRoom())
-
-               //if the destination of the room you are entering is called "EXIT" then you have either won or lost the game
-               if (currentRoomPassages[indexOfForcedKey].getDestinationRoom() === "EXIT") {
-                  gameStatus = "Game Over"
-                  return
                }
-               currentRoom = rooms[currentRoomPassages[indexOfForcedKey].getDestinationRoom()]
-            }  
+            }
+
+            //if a person doesn't have a key then the forced index is one. All forced passages with a key have an index of 0
+            else {
+               indexOfForcedKey = 1
+           }
+         } 
+         // console.log("this is the index of the forced passage " + indexOfForcedKey)
+         // console.log("this is the current room " + currentRoomPassages[indexOfForcedKey].getDestinationRoom())
+
+         //if the destination of the room you are entering is called "EXIT" then you have either won or lost the game
+         if (currentRoomPassages[indexOfForcedKey].getDestinationRoom() === "EXIT") {
+            gameStatus = "Game Over"
+            return
+         }
+         currentRoom = rooms[currentRoomPassages[indexOfForcedKey].getDestinationRoom()]
+
+         if (currentRoom.getPassages()[0].getDirection() === "forced") {
+            currentRoomPassages = currentRoom.getPassages()
+            forcedPassages(currentRoomPassages, inventory, rooms )
+         } 
+
+      } 
    }
 
    //this function contains the commands that are run if a specific action word is typed in by the user
@@ -261,6 +282,11 @@ function AdvGame() {
 
          case "inventory":
             printInventory(inventory)
+            console.requestInput("> ", checkAnswer);
+            break;
+
+         case "cheat":
+            cheatCode(objects,inventory);
             console.requestInput("> ", checkAnswer);
             break;
             
@@ -315,7 +341,16 @@ function takeObject(objectInput,objects,currentRoom,inventory) {
    let arrayOfObjects = Object.values(objects)
    let arrayOfObjectNames = Object.keys(objects)
 
-   objectInput = objectInput.toUpperCase() //turn the input into uppercase so it can match the names of the objects
+   //if there was in face an input then you can make it capital letters
+   if (objectInput !== undefined) {
+      objectInput = objectInput.toUpperCase() //turn the input into uppercase so it can match the names of the objects
+   }
+   
+   //if there isnt an input then write an error message
+   if (objectInput === undefined) {
+      console.log("You didn't finish writing your sentence! What object should I take?")
+      return
+   }
    let indexOfObject //initialize variable now
 
    //this loop allows me to find the index of the object according to its name
@@ -340,10 +375,20 @@ function takeObject(objectInput,objects,currentRoom,inventory) {
 
 //this function allows you to drop an item from your inventory into the current room the player is in
 function dropObject(objectInput,currentRoom, objects, inventory) {
-   objectInput = objectInput.toUpperCase() //make sure the input is capital so that it matches the names of the objects
    let arrayOfObjects = Object.values(objects)
    let arrayOfObjectNames = Object.keys(objects)
    let indexOfObject
+
+   //if there was in face an input then you can make it capital letters
+   if (objectInput !== undefined) {
+      objectInput = objectInput.toUpperCase() //turn the input into uppercase so it can match the names of the objects
+   }
+
+   //if there isnt an input then write an error message
+   if (objectInput === undefined) {
+      console.log("You didn't finish writing your sentence! What object should I drop?")
+      return
+   }
 
    //this loop allows us to find the index of the object according to its name
    for (let i=0; i<arrayOfObjectNames.length; i++) {
@@ -386,7 +431,7 @@ function printInventory(inventory) {
    }
 }
 
-function distributeObjects(objects,rooms) {
+function distributeObjects(objects,rooms,inventory) {
    //use a for loop.. loop through the objects
    //first read the post it and the location name
    // then push the object into the room when location matches room name //!! PLAYER is not a room!
@@ -398,14 +443,28 @@ function distributeObjects(objects,rooms) {
 
    for (let i=0; i<numOfObjects; i++) {
       let locationsOfObjects = arrayOfObjects[i].getLocation() //this takes the location from an object at an index
+
+      //first will distribute any objects belonging to the player straight into their inventory
+      if (locationsOfObjects === "PLAYER") {
+         inventory.push(arrayOfObjects[i])
+      }
+
+      //loop through my array of rooms. If the location of the object matches the name of the room, then I will add that object to that room
       for (let j=0; j<arrayOfRoomNames.length; j++) {
          let nameOfRoom = arrayOfRoomNames[j]
-
          if (locationsOfObjects === nameOfRoom) {
             arrayOfRoomObjects[j].addObject(arrayOfObjects[i])
          }
       }
-   }   
+   } 
+}
+
+function cheatCode(objects,inventory) {
+   console.log("you're cheating")
+   let arrayOfObjects = Object.values(objects)
+   for (let i=0; i<arrayOfObjects.length; i++) {
+      inventory.push(arrayOfObjects[i])
+   }
 }
 
 //tbis function creates a synonym object 
